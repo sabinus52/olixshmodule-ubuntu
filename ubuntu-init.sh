53b839e71d88daa8a8df5554e1f7c228160b4e6d
Olivier <sabinus52@gmail.com>
2015-04-19 12:50:52 +0200

    Développement de l'action "init"

---
+++ ubuntu-init.sh
@@ -0,0 +1,48 @@
+
+function olixmod_ubuntu_init()
+{
+    logger_debug "module_ubuntu__olixmod_ubuntu_init ($@)"
+
+    local FORCE=false
+    while [[ $# -ge 1 ]]; do
+        case $1 in
+            --force|-f) FORCE=true;;
+        esac
+        shift
+    done
+
+    local FILECONF=$(config_getFilenameModule ${OLIX_MODULE_NAME})
+
+    # Test si la configuration existe
+    logger_info "Test si la configuration est déjà effectuée"
+    if config_isModuleExist ${OLIX_MODULE_NAME} && [[ ${FORCE} == false ]] ; then
+        logger_warning "Le fichier de configuration existe déjà"
+        if [[ ${OLIX_OPTION_VERBOSE} == true ]]; then
+            echo "----------"
+            cat ${FILECONF}
+            echo "----------"
+        fi
+        logger_warning "Pour reinitialiser la configuration, utiliser : ${OLIX_CORE_SHELL_NAME} ubuntu init -f|--force"    
+        core_exit 0
+    fi
+
+    # Test si c'est le propriétaire
+    logger_info "Test si c'est le propriétaire"
+    core_checkIfOwner
+    [[ $? -ne 0 ]] && logger_error "Seul l'utilisateur \"$(core_getOwner)\" peut exécuter ce script"
+
+    if config_isModuleExist ${OLIX_MODULE_NAME}; then
+        logger_info "Chargement du fichier de configuration ${FILECONF}"
+        source ${FILECONF}
+    fi
+
+    # Demande du fichier
+    stdin_readFile "Chemin complet du fichier contenant la configuration de l'installation du serveur" "${OLIX_MODULE_UBUNTU_CONFIG}"
+    logger_debug "module_ubuntu__olixmod__init RESPONSE=${OLIX_STDIN_RETURN}"
+   
+    # Ecriture du fichier de configuration
+    logger_info "Création du fichier de configuration ${FILECONF}"
+    logger_debug "OLIX_MODULE_UBUNTU_CONFIG=${OLIX_STDIN_RETURN}"
+    echo "# Fichier de configuration pour l'install d'Ubuntu" > ${FILECONF}
+    echo "OLIX_MODULE_UBUNTU_CONFIG=${OLIX_STDIN_RETURN}" >> ${FILECONF}
+}
