04a7eb1eeac0899f3c6efd0b8caf3b63929eaa08
Olivier <sabinus52@gmail.com>
2015-04-20 21:47:25 +0200

    Service "apache" en version 14.04

---
+++ 14.04/apache.inc.sh
@@ -0,0 +1,168 @@
+###
+# Installation et configuration d'APACHE
+# ==============================================================================
+# - Installation des paquets APACHE
+# - Installation de la clé privée
+# - Activation des modules
+# - Installation des fichiers de configuration
+# ------------------------------------------------------------------------------
+# apache:
+#    enabled: (OLIX_MODULE_UBUNTU_APACHE__ENABLED)
+#    modules: (OLIX_MODULE_UBUNTU_APACHE__MODULES) Liste des modules apache à activer
+#    configs: (OLIX_MODULE_UBUNTU_APACHE__CONFIGS) Liste des fichiers de configuration
+#    default: (OLIX_MODULE_UBUNTU_APACHE__DEFAULT) Site par défaut
+# ------------------------------------------------------------------------------
+# @package olixsh
+# @module ubuntu
+# @author Olivier <sabinus52@gmail.com>
+# @version 14.04
+##
+
+
+ubuntu_include_title()
+{
+    echo
+    echo -e "${CBLANC} Installation et Configuration de APACHE ${CVOID}"
+    echo -e "-------------------------------------------------------------------------------"
+}
+
+
+###
+# Fonction principal
+# @param $1 : action à faire
+##
+ubuntu_include_main()
+{
+    logger_debug "ubuntu_include_main (apache, $1)"
+
+    if [[ "${OLIX_MODULE_UBUNTU_APACHE__ENABLED}" != true ]]; then
+        logger_warning "Service 'apache' non activé"
+        return 1
+    fi
+
+    __PATH_CONFIG="$(dirname ${OLIX_MODULE_UBUNTU_CONFIG})/apache"
+
+    case $1 in
+        install)
+            ubuntu_include_install
+            ubuntu_include_config
+            ubuntu_include_restart
+            ;;
+        config)
+            ubuntu_include_config
+            ubuntu_include_restart
+            ;;
+        restart)
+            ubuntu_include_restart
+            ;;
+    esac
+}
+
+
+###
+# Installation du service
+##
+ubuntu_include_install()
+{
+    logger_debug "ubuntu_include_install (apache)"
+
+    logger_info "Installation des packages APACHE"
+    apt-get --yes install apache2-mpm-prefork ssl-cert
+    [[ $? -ne 0 ]] && logger_error "Impossible d'installer les packages APACHE"
+}
+
+
+###
+# Configuration du service
+##
+ubuntu_include_config()
+{
+    logger_debug "ubuntu_include_config (apache)"
+
+    # Activation des modules Apache
+    ubuntu_include_apache_modules
+    # Installation des fichiers de configuration
+    ubuntu_include_apache_configs
+    # Activation du site par défaut
+    ubuntu_include_apache_default
+}
+
+
+###
+# Redemarrage du service
+##
+ubuntu_include_restart()
+{
+    logger_debug "ubuntu_include_restart (apache)"
+
+    logger_info "Redémarrage du service APACHE"
+    service apache2 restart
+    [[ $? -ne 0 ]] && logger_error
+}
+
+
+###
+# Activation des modules Apache
+##
+function ubuntu_include_apache_modules()
+{
+    logger_debug "ubuntu_include_apache_modules ()"
+
+    for I in ${OLIX_MODULE_UBUNTU_APACHE__MODULES}; do
+        logger_info "Activation du module $I"
+        a2enmod $I > ${OLIX_LOGGER_FILE_ERR} 2>&1
+        [[ $? -ne 0 ]] && logger_error
+        echo -e "Activation du module ${CCYAN}$I${CVOID} : ${CVERT}OK ...${CVOID}"
+    done
+}
+
+
+###
+# Installation des fichiers de configuration
+##
+function ubuntu_include_apache_configs()
+{
+    logger_debug "ubuntu_include_apache_configs ()"
+
+    logger_info "Suppression de la conf actuelle"
+    rm -rf /etc/apache2/conf-enabled/* > ${OLIX_LOGGER_FILE_ERR} 2>&1
+    [[ $? -ne 0 ]] && logger_error
+    rm -rf /etc/apache2/conf-available/olix* > ${OLIX_LOGGER_FILE_ERR} 2>&1
+    [[ $? -ne 0 ]] && logger_error
+    for I in $(ls ${__PATH_CONFIG}/conf/olix*); do
+        module_ubuntu_installFileConfiguration "$I" "/etc/apache2/conf-available/"
+    done
+    for I in ${OLIX_MODULE_UBUNTU_APACHE__CONFIGS}; do
+        logger_info "Activation de la conf $I"
+        a2enconf $I > ${OLIX_LOGGER_FILE_ERR} 2>&1
+        [[ $? -ne 0 ]] && logger_error
+        echo -e "Activation de la conf ${CCYAN}$I${CVOID} : ${CVERT}OK ...${CVOID}"
+    done
+}
+
+
+###
+# Activation du site par défaut
+##
+function ubuntu_include_apache_default()
+{
+    logger_debug "ubuntu_include_apache_default ()"
+
+    if [[ -z ${OLIX_MODULE_UBUNTU_APACHE__DEFAULT} ]]; then
+        logger_warning "Pas de site par défaut défini"
+        return 1
+    fi
+
+    module_ubuntu_backupFileOriginal "/etc/apache2/sites-available/000-default.conf"
+
+    logger_info "Effacement de /etc/apache2/sites-enabled/000-default.conf"
+    rm -rf /etc/apache2/sites-enabled/000-default.conf > ${OLIX_LOGGER_FILE_ERR} 2>&1
+    [[ $? -ne 0 ]] && logger_error
+
+    module_ubuntu_installFileConfiguration "${__PATH_CONFIG}/default/${OLIX_MODULE_UBUNTU_APACHE__DEFAULT}" "/etc/apache2/sites-available/000-default.conf"
+
+    logger_info "Activation du site 000-default.conf"
+    a2ensite 000-default.conf > ${OLIX_LOGGER_FILE_ERR} 2>&1
+    [[ $? -ne 0 ]] && logger_error
+    echo -e "Activation du site ${CCYAN}default.conf${CVOID} : ${CVERT}OK ...${CVOID}"
+}
